96e050e5b039484c54e6571bd28fc16ab6b6b90b
robertoDeSPb <86190010+robertoDeSPb@users.noreply.github.com>
2025-10-16 09:14:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-# class house:
-#     floors = 3
-#     default_color = "grey"
-
-#     def __init__(self, len, width):
-#         self.len = len
-#         self.width = width
-    
-#     def add_floor(self):
-#         self.floors += 1
-#     def get_floors_count(self):
-#         return self.floors
-#     def get_square(self):
-#         return self.len * self.width * int(self.floors)
-
-
-
-
-# x, y = [int(x) for x in input().split()]
-
-# h1 = house(x,y)
-# print(h1.get_square())
-
-# h1.add_floor()
-
-# print(h1.get_square())
-
 from psycopg2 import OperationalError
 import psycopg2
 
@@ -52,16 +25,6 @@ def create_database(connection, query):
         print("Query executed successfully")
     except OperationalError as e:
         print(f"The error '{e}' occured")
-
-
-
-# connection = create_connection(
-#     "postgres", "postgres", "1", "127.0.0.1", "5432"
-# )
-
-# create_database_query = "CREATE DATABASE sm_app"
-
-#create_database(connection, create_database_query)
 
 connection = create_connection(
     "sm_app", "postgres", "1", "127.0.0.1", "5432"
